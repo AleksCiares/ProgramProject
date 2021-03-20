432742cbd595f14d72d0d112e076eecc7e8184f6
Pavel Tamashevich <paveltom99@gmail.com>
2021-03-20 10:31:47 +0300

    First init commit

---
+++ ClientService/DLPClientService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientService
+{
+    public partial class DLPClientService : ServiceBase
+    {
+        private StreamWriter file;
+
+        public DLPClientService()
+        {
+            InitializeComponent();
+            //eventLog1 = new System.Diagnostics.EventLog();
+            //if(!System.Diagnostics.EventLog.SourceExists("MySource"))
+            //{
+            //    System.Diagnostics.EventLog.CreateEventSource("MySource", "MyNewLog");
+            //}
+            //eventLog1.Source = "MySource";
+            //eventLog1.Log = "MyNewLog";
+
+            //this.CanStop = false;
+            //this.CanPauseAndContinue = false;
+
+            this.file = new StreamWriter(new FileStream("F:\\MyFirstService.log",
+                System.IO.FileMode.Append));
+            this.file.WriteLine("Service started");
+            this.file.Flush();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //DateTime localDate = DateTime.Now;
+            //String cultureName = "be-BY";
+            //eventLog1.WriteEntry($"{cultureName} {localDate.ToString(new CultureInfo(cultureName))} :" +
+            //    $"service started");
+
+            this.file.Write("Service stopped");
+            this.file.Flush();
+            this.file.Close();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ WebServer/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebServer.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Please enter valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please precify wheter you`ll attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
